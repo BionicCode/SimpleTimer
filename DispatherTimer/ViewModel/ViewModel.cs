340f93cbc866f44d470c4771c175b710a0401350
vadimffe <vadim.ffegmail.com>
2020-11-28 17:03:55 +0200

    Add project files.

---
+++ DispatherTimer/ViewModel/ViewModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Threading;
+
+namespace DispatherTimer
+{
+    class ViewModel : BaseViewModel
+    {
+        public ViewModel()
+        {
+            StartWorkingTimeTodayTimer();
+        }
+
+        /// <summary>
+        /// Timer implementation, working time today
+        /// <summary>
+        private string _currentTime;
+
+        public string CurrentTime
+        {
+            get
+            {
+                return this._currentTime;
+            }
+            set
+            {
+                if (_currentTime == value)
+                    return;
+                _currentTime = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private static DateTime StartTimeWholeDay;
+        private DispatcherTimer _dailyTimer;
+        public void StartWorkingTimeTodayTimer()
+        {
+            StartTimeWholeDay = DateTime.Now;
+            DateTime x30MinsLater = StartTimeWholeDay.AddSeconds(50);
+
+            _dailyTimer = new DispatcherTimer(DispatcherPriority.Render);
+            _dailyTimer.Interval = TimeSpan.FromSeconds(1);
+            _dailyTimer.Tick += (sender, args) =>
+            {
+                CurrentTime = (DateTime.Now - StartTimeWholeDay).ToString(@"hh\:mm\:ss"); // DateTime.Now.ToLongTimeString()
+            };
+            _dailyTimer.Start();
+        }
+    }
+}
